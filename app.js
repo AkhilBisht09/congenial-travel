6fe0731ef0b4c82b912666587d056b73dde1866b
Paras Gupta <parasg1999@gmail.com>
2019-03-09 10:16:36 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,51 @@
+var express               =require("express"),
+    mongoose              =require("mongoose"),
+    passport              =require("passport"),
+    User                  =require("./models/user"),
+    LocalStrategy         =require("passport-local"),
+    bodyParser            =require("body-parser"),
+    app                   =express(),
+    passportLocalMongoose =require("passport-local-mongoose");
+
+const authRoutes = require("./routes/auth");
+
+mongoose.connect("mongodb+srv://Vedaant:vedaant123@cluster0-yunv0.mongodb.net/test?retryWrites=true");
+app.use(require("express-session")({
+    secret:"Rusty is the cutest",
+    resave:false,
+    saveUninitialized: false,
+    
+}));
+app.set('view engine','ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+///================
+///   ROUTES
+///================
+
+app.use("/auth", authRoutes);
+
+app.get("/secret",isLoggedIn ,function(req,res){
+    res.render("secret");
+});
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/auth/login");
+}
+var port = process.env.PORT || 3000;
+app.listen(port,function(){
+    console.log("Serving on port", port);
+});
---
+++ routes/auth.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var router = express.Router();
+var passport = require("passport");
+
+//show sign up form
+router.get("/register",function(req,res){
+    res.render("register");
+});
+//user sign up
+router.post("/register", function(req,res){
+    /* req.body.username
+    req.body.password */
+    User.register(new User({username:req.body.username}),req.body.password,function(err,user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+
+        }
+        passport.authenticate("local")(req,res,function(){
+            res.redirect("/secret");
+        });
+    });
+});
+
+///Login forms
+
+router.get("/login",function(req,res){
+    res.render("login");
+});
+
+
+//login login
+
+//middleware
+router.post("/login", passport.authenticate("local",{
+    successRedirect: "/secret",
+    failureRedirect: "/auth/login"
+}),
+    function(req,res){
+});
+
+router.get("/logout",function(req,res){
+    req.logout();    ///destroying user session
+    res.redirect("/");
+});
+
+module.exports = router;

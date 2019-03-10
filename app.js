1e377f1e7f85f36bf4dffd24fa50297533974491
Paras Gupta <parasg1999@gmail.com>
2019-03-10 12:03:10 +0530

    Chat DONE

--- app.js
+++ app.js
@@ -7,10 +7,18 @@ var express               =require("express"),
     app                   =express(),
     passportLocalMongoose =require("passport-local-mongoose");
 
+const http = require('http');
+const socketIO = require('socket.io');
+
+const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
+
 const authRoutes = require("./routes/auth");
 const mapRoutes = require("./routes/map");
+var users = new Users();
 
-mongoose.connect("mongodb+srv://Vedaant:vedaant123@cluster0-yunv0.mongodb.net/test?retryWrites=true");
+mongoose.connect("mongodb+srv://Vedaant:vedaant123@cluster0-yunv0.mongodb.net/test?retryWrites=true", {useNewUrlParser: true});
 app.use(require("express-session")({
     secret:"Rusty is the cutest",
     resave:false,
@@ -33,12 +41,65 @@ passport.deserializeUser(User.deserializeUser());
 app.use("/auth", authRoutes);
 app.use("/map", mapRoutes);
 
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.get("/secret",isLoggedIn ,function(req,res){
-    res.render("secret");
+    res.render("dashboard");
 });
 
 app.get("/",function(req,res){
     res.render("home");
+});
+
+app.get("/chat", function(req, res) {
+    res.render("chat");
+});
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('join', (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      return callback('Name and room name are required.');
+    }
+
+    socket.join(params.room);
+    users.removeUser(socket.id);
+    users.addUser(socket.id, params.name, params.room);
+
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
+    callback();
+  });
+
+  socket.on('createMessage', (message, callback) => {
+    var user = users.getUser(socket.id);
+
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    }
+
+    callback();
+  });
+
+  socket.on('createLocationMessage', (coords) => {
+    var user = users.getUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));  
+    }
+  });
+
+  socket.on('disconnect', () => {
+    var user = users.removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+      io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
+    }
+  });
 });
 
 function isLoggedIn(req, res, next){
@@ -47,7 +108,8 @@ function isLoggedIn(req, res, next){
     }
     res.redirect("/auth/login");
 }
+
 var port = process.env.PORT || 3000;
-app.listen(port,function(){
+server.listen(port,function(){
     console.log("Serving on port", port);
 });
